1924c4d3a4530c6a25084adb09dfd1b290656638
SnyprDragun <subho02.dc@gmail.com>
2024-08-25 14:20:31 +0000

    test

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/subho/catkin_w_s/src/dd_stl_stt/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/subho/catkin_w_s/src/dd_stl_stt/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/subho/catkin_w_s/src/dd_stl_stt/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/subho/catkin_w_s/src/dd_stl_stt/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/subho/catkin_w_s/src/dd_stl_stt/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH='/home/subho/catkin_w_s/src/dd_stl_stt:/home/subho/catkin_w_s/src/mavlink:/home/subho/catkin_w_s/src/mavros/libmavconn:/home/subho/catkin_w_s/src/mavros/mavros_msgs:/home/subho/catkin_w_s/src/control_scripts:/home/subho/catkin_w_s/src/mavros/mavros:/home/subho/catkin_w_s/src/mavros/mavros_extras:/home/subho/catkin_w_s/src/quadcopter_perception:/home/subho/catkin_w_s/src/mavros/test_mavros:/opt/ros/noetic/share'
+export ROS_PACKAGE_PATH='/home/subho/catkin_w_s/src/dd_stl_stt:/home/subho/catkin_w_s/src/dd_stl_stt:/home/subho/catkin_w_s/src/mavlink:/home/subho/catkin_w_s/src/mavros/libmavconn:/home/subho/catkin_w_s/src/mavros/mavros_msgs:/home/subho/catkin_w_s/src/control_scripts:/home/subho/catkin_w_s/src/mavros/mavros:/home/subho/catkin_w_s/src/mavros/mavros_extras:/home/subho/catkin_w_s/src/quadcopter_perception:/home/subho/catkin_w_s/src/mavros/test_mavros:/opt/ros/noetic/share'
--- src/tube_nav.py
+++ src/tube_nav.py
@@ -2,52 +2,60 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-import math
+import time
 
-def sine_wave_motion():
+def follow_equation(C0, C1, C2, final_time):
     # Initialize the ROS node
-    rospy.init_node('turtlebot_sine_wave', anonymous=True)
+    rospy.init_node('turtlebot_follow_equation', anonymous=True)
     
-    # Create a publisher to the '/cmd_vel' topic to control the robot's velocity
+    # Publisher to send velocity commands to the TurtleBot
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     
-    # Set the loop rate (Hz)
-    rate = rospy.Rate(10)
+    # Set the rate at which to loop
+    rate = rospy.Rate(10)  # 10 Hz
     
-    # Parameters for the sine wave
-    amplitude = 1.0   # Amplitude of the sine wave (meters)
-    frequency = 0.2   # Frequency of the sine wave (Hz)
-    linear_speed = 0.5  # Constant linear speed along the x-axis (meters/second)
-    
+    # Record the start time
     start_time = rospy.Time.now().to_sec()
-    
+
     while not rospy.is_shutdown():
-        # Calculate the current time
-        current_time = rospy.Time.now().to_sec()
+        # Get the current time
+        current_time = rospy.Time.now().to_sec() - start_time
         
-        # Time elapsed since the start of the program
-        elapsed_time = current_time - start_time
+        # Calculate the desired position using the given equation
+        x_desired = C0 + C1 * current_time + C2 * current_time**2
         
-        # Calculate the desired y position using a sine function
-        y_position = amplitude * math.sin(2 * math.pi * frequency * elapsed_time)
+        # Calculate the derivative to find the desired velocity
+        x_velocity = C1 + 2 * C2 * current_time
         
-        # Calculate the desired y velocity (derivative of the sine function)
-        y_velocity = 2 * math.pi * frequency * amplitude * math.cos(2 * math.pi * frequency * elapsed_time)
-        
-        # Create a Twist message and populate it
+        # Create a Twist message to send velocity commands
         vel_msg = Twist()
-        vel_msg.linear.x = linear_speed
-        vel_msg.linear.y = y_velocity
-        vel_msg.angular.z = 0.0  # No rotation
         
-        # Publish the velocity message
+        # Set the linear x velocity (assuming movement along the x-axis)
+        vel_msg.linear.x = x_velocity
+        
+        # We assume no rotation is needed
+        vel_msg.angular.z = 0.0
+        
+        # Publish the velocity command
         cmd_vel_pub.publish(vel_msg)
+        
+        # Stop the robot if the final time is reached
+        if current_time >= final_time:
+            vel_msg.linear.x = 0.0
+            cmd_vel_pub.publish(vel_msg)
+            break
         
         # Sleep to maintain the loop rate
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        sine_wave_motion()
+        # Define your coefficients and final time
+        C0 = 0.0
+        C1 = 0.3
+        C2 = 0.02
+        final_time = 10.0  # Time at which x = 5
+        
+        follow_equation(C0, C1, C2, final_time)
     except rospy.ROSInterruptException:
         pass
