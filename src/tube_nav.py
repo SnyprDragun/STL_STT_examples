f520c05af9ea00737463df8182bdf81db8bbc12d
SnyprDragun <subho02.dc@gmail.com>
2024-08-26 16:15:34 +0000

    hj

--- src/tube_nav.py
+++ src/tube_nav.py
@@ -14,11 +14,11 @@ class TurtleBot:
         rospy.init_node('turtlebot_controller', anonymous=True)
 
         # Publisher which will publish to the topic '/turtle1/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
         # when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+        self.pose_subscriber = rospy.Subscriber('/imu', Pose, self.update_pose)
 
         self.pose = Pose()
         self.rate = rospy.Rate(10)
@@ -31,8 +31,8 @@ class TurtleBot:
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
-                    pow((goal_pose.y - self.pose.y), 2))
+        return sqrt(pow((goal_pose.x - self.pose.x), 2) +pow((goal_pose.y - self.pose.y), 2))
+                    
 
     def linear_vel(self, goal_pose, constant=1.5):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
