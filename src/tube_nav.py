937bdcd2762bf8156a5eca714f0fa3b404f9a4d8
SnyprDragun <subho02.dc@gmail.com>
2024-08-26 10:37:58 +0000

    try1

--- src/tube_nav.py
+++ src/tube_nav.py
@@ -1,26 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
+import numpy as np
 from geometry_msgs.msg import Twist
-import time
 
-def follow_equation(C0, C1, C2, final_time):
-    rospy.init_node('turtlebot_follow_equation', anonymous=True)
+def gammas(degree, dimension, t):
+        '''method to calculate tube equations'''
+        C = []
+        
+        vel_tubes = np.zeros(2 * dimension)
+        for i in range(2 * dimension): #for 4 tube equations
+            power = 0
+            for j in range(degree + 1): #each tube eq has {degree+1} terms
+                if power > 0:
+                    vel_tubes[i] += power * ((C[j + i * (degree + 1)]) * (t ** (power - 1)))
+                else:
+                    vel_tubes[i] += 0
+                power += 1
+        gamma1_L, gamma2_L, gamma1_U, gamma2_U = vel_tubes[0], vel_tubes[1], vel_tubes[2], vel_tubes[3]
+        x_vel = (gamma1_L + gamma1_U)/2
+        y_vel = (gamma2_L + gamma2_U)/2
+        return x_vel, y_vel
+
+def follow_equation(start_time, end_time):
+    rospy.init_node('stt_follower', anonymous=True)
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
     start_time = rospy.Time.now().to_sec()
 
     while not rospy.is_shutdown():
         current_time = rospy.Time.now().to_sec() - start_time
-        x_desired = C0 + C1 * current_time + C2 * current_time**2
-        x_velocity = C1 + 2 * C2 * current_time
-
+        x_vel , y_vel = gammas(2, 2, current_time)
         vel_msg = Twist()
-        vel_msg.linear.x = x_velocity
+        vel_msg.linear.x = x_vel
+        vel_msg.linear.y = y_vel
         vel_msg.angular.z = 0.0
         cmd_vel_pub.publish(vel_msg)
 
-        if current_time >= final_time:
+        if current_time >= end_time:
             vel_msg.linear.x = 0.0
             cmd_vel_pub.publish(vel_msg)
             break
@@ -28,12 +45,4 @@ def follow_equation(C0, C1, C2, final_time):
         rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        C0 = 0.0
-        C1 = 0.3
-        C2 = 0.02
-        final_time = 10.0
-        
-        follow_equation(C0, C1, C2, final_time)
-    except rospy.ROSInterruptException:
-        pass
+    follow_equation(0, 10)
