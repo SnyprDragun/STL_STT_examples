4181534bf0c11e543c921a316dcc7e9c9b3c49d1
SnyprDragun <subho02.dc@gmail.com>
2024-08-27 13:49:36 +0000

    testing

--- src/tube_nav.py
+++ src/tube_nav.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
@@ -14,25 +13,26 @@ class TurtleBot:
         rospy.init_node('turtlebot_controller', anonymous=True)
 
         # Publisher which will publish to the topic '/turtle1/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
         # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
         # when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('/imu', Pose, self.update_pose)
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
 
         self.pose = Pose()
         self.rate = rospy.Rate(10)
 
     def update_pose(self, data):
-        """Callback function which is called when a new message of type Pose is received by the subscriber."""
+        """Callback function which is called when a new message of type Pose is
+        received by the subscriber."""
         self.pose = data
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.x - self.pose.x), 2) +pow((goal_pose.y - self.pose.y), 2))
-                    
+        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
+                    pow((goal_pose.y - self.pose.y), 2))
 
     def linear_vel(self, goal_pose, constant=1.5):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
@@ -46,16 +46,16 @@ class TurtleBot:
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
         return constant * (self.steering_angle(goal_pose) - self.pose.theta)
 
-    def move2goal(self, x, y, tolerance):
+    def move2goal(self):
         """Moves the turtle to the goal."""
         goal_pose = Pose()
 
         # Get the input from the user.
-        goal_pose.x = x
-        goal_pose.y = y
+        goal_pose.x = float(input("Set your x goal: "))
+        goal_pose.y = float(input("Set your y goal: "))
 
         # Please, insert a number slightly greater than 0 (e.g. 0.01).
-        distance_tolerance = tolerance
+        distance_tolerance = input("Set your tolerance: ")
 
         vel_msg = Twist()
 
@@ -72,7 +72,7 @@ class TurtleBot:
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = 0.0
+            vel_msg.angular.z = self.angular_vel(goal_pose)
 
             # Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
@@ -90,7 +90,7 @@ class TurtleBot:
 
 if __name__ == '__main__':
     try:
-        bot = TurtleBot()
-        bot.move2goal(5, 5, 0.01)
+        x = TurtleBot()
+        x.move2goal()
     except rospy.ROSInterruptException:
         pass
