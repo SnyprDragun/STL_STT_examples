9769ada24b9e26957b6f15a2c5d71306bc3de914
SnyprDragun <subho02.dc@gmail.com>
2024-08-27 15:20:53 +0000

    off

--- src/tube_nav.py
+++ src/tube_nav.py
@@ -1,96 +1,65 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
-from math import pow, atan2, sqrt
+from geometry_msgs.msg  import Twist
+from nav_msgs.msg import Odometry
+from math import pow,atan2,sqrt,sin,cos
+from tf.transformations import euler_from_quaternion
+import numpy as np 
 
-
-class TurtleBot:
-
+class turtlebot():
     def __init__(self):
-        # Creates a node with name 'turtlebot_controller' and make sure it is a
-        # unique node (using anonymous=True).
+        #Creating our node,publisher and subscriber
         rospy.init_node('turtlebot_controller', anonymous=True)
-
-        # Publisher which will publish to the topic '/turtle1/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-
-        # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
-        # when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
-
-        self.pose = Pose()
+        self.velocity_publisher = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('/odom', Odometry, self.callback)
+        self.pose = Odometry()
         self.rate = rospy.Rate(10)
 
-    def update_pose(self, data):
-        """Callback function which is called when a new message of type Pose is
-        received by the subscriber."""
-        self.pose = data
+    #Callback function implementing the pose value received
+    def callback(self, data):
+        self.pose = data.pose.pose.position
+        self.orient = data.pose.pose.orientation
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
 
-    def euclidean_distance(self, goal_pose):
-        """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
-                    pow((goal_pose.y - self.pose.y), 2))
-
-    def linear_vel(self, goal_pose, constant=1.5):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * self.euclidean_distance(goal_pose)
-
-    def steering_angle(self, goal_pose):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
-
-    def angular_vel(self, goal_pose, constant=6):
-        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
-
     def move2goal(self):
-        """Moves the turtle to the goal."""
-        goal_pose = Pose()
-
-        # Get the input from the user.
-        goal_pose.x = float(input("Set your x goal: "))
-        goal_pose.y = float(input("Set your y goal: "))
-
-        # Please, insert a number slightly greater than 0 (e.g. 0.01).
-        distance_tolerance = float(input("Set your tolerance: "))
-
+        K1=0.5
+        K2=0.5
+        goal_pose_ = Odometry()
+        goal_pose = goal_pose_.pose.pose.position
+        goal_pose.x = input("Set your x goal:")
+        goal_pose.y = input("Set your y goal:")
+        distance_tolerance = input("Set your tolerance:")
         vel_msg = Twist()
+        r = sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
+        while r >= distance_tolerance:
 
-        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+            r = sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
+            psi = atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+            orientation_list = [self.orient.x, self.orient.y, self.orient.z, self.orient.w]
+            (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+            theta = yaw
+            phi = theta - psi
+            if phi > np.pi:
+                phi = phi - 2*np.pi
+            if phi < -np.pi:
+                phi = phi + 2*np.pi
 
-            # Porportional controller.
-            # https://en.wikipedia.org/wiki/Proportional_control
+            vel_msg.linear.x = K1*r*cos(phi)
+            vel_msg.angular.z = -K1*sin(phi)*cos(phi)-(K2*phi)
 
-            # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(goal_pose)
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-
-            # Angular velocity in the z-axis.
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = self.angular_vel(goal_pose)
-
-            # Publishing our vel_msg
+            #Publishing input
             self.velocity_publisher.publish(vel_msg)
-
-            # Publish at the desired rate.
             self.rate.sleep()
-
-        # Stopping our robot after the movement is over.
+        #Stopping our robot after the movement is over
         vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
+        vel_msg.angular.z =0
         self.velocity_publisher.publish(vel_msg)
 
-        # If we press control + C, the node will stop.
-        rospy.spin()
-
 if __name__ == '__main__':
-    try:
-        x = TurtleBot()
+   x = turtlebot()
+   while 1:
+      try:
         x.move2goal()
-    except rospy.ROSInterruptException:
+      except:
         pass
