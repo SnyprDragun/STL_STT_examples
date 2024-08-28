bfce3aeac010023e0706ab5d746096a43c8b7004
SnyprDragun <subho02.dc@gmail.com>
2024-08-28 11:33:03 +0000

    fdg

--- src/tube_nav.py
+++ src/tube_nav.py
@@ -7,13 +7,16 @@ from tf.transformations import euler_from_quaternion
 import numpy as np 
 
 class turtlebot():
-    def __init__(self):
+    def __init__(self, C, degree):
         #Creating our node,publisher and subscriber
         rospy.init_node('turtlebot_controller', anonymous=True)
         self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.pose_subscriber = rospy.Subscriber('/odom', Odometry, self.callback)
         self.pose = Odometry()
         self.rate = rospy.Rate(20)
+        self.C = C
+        self.dimension = 2
+        self.degree = degree
 
     #Callback function implementing the pose value received
     def callback(self, data):
@@ -22,14 +25,28 @@ class turtlebot():
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
 
-    def move2goal(self, x, y, tol):
-        K1 = 0.4
+    def gamma(self, t):
+        real_tubes = np.zeros(2 * self.dimension)
+
+        for i in range(2 * self.dimension): #for 4 tube equations
+            power = 0
+            for j in range(self.degree + 1): #each tube eq has {degree+1} terms
+                real_tubes[i] += ((self.C[j + i * (self.degree + 1)]) * (t ** power))
+                power += 1
+        x_l = real_tubes[0]
+        y_l = real_tubes[1]
+        x_u = real_tubes[2]
+        y_u = real_tubes[3]
+        return (x_u + x_l)/2, (y_u + y_l)/2
+
+    def move2goal(self, x, y):
+        K1 = 0.2
         K2 = 0.1
         goal_pose_ = Odometry()
         goal_pose = goal_pose_.pose.pose.position
         goal_pose.x = x
         goal_pose.y = y
-        distance_tolerance = tol
+        distance_tolerance = 0.01
         vel_msg = Twist()
         r = sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
         while r >= distance_tolerance:
@@ -56,10 +73,16 @@ class turtlebot():
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
 
+    def traverse(self):
+        for time in np.arange(0, 10.05, 0.05):
+            x_target, y_target = self.gamma(time)
+            self.move2goal(x_target, y_target)
+
+
 if __name__ == '__main__':
    x = turtlebot()
    while 1:
       try:
-        x.move2goal(5, 5, 0.01)
+        x.traverse()
       except:
         pass
