bc33894bc99098528ae51f56e10af6cbb33a6e57
Subhodeep Choudhury <105502718+SnyprDragun@users.noreply.github.com>
2024-09-02 11:52:40 +0530

    Add files via upload

---
+++ src/scripts/root_controller.cpp
@@ -0,0 +1,49 @@
+#include "stl_stt_examples/takeoff_node.hpp"
+#include "stl_stt_examples/offboard_node.hpp"
+#include "stl_stt_examples/landing_node.hpp"
+
+mavros_msgs::State current_state;
+mavros_msgs::SetMode set_mode;
+
+int main(int argc, char **argv){
+    init(argc, argv, "root_controller", init_options::AnonymousName);
+    
+    ROS_INFO("Initializing Root Controller...");
+
+    float altitude = 1.0;
+
+    Takeoff* takeoff = new Takeoff();
+    takeoff->init_connection();
+    takeoff->arm();
+    takeoff->takeoff(altitude);
+
+    Time last_request = Time::now();
+
+    bool flag = true;
+    while (flag){
+        if (current_state.mode != "AUTO.LOITER" && (Time::now() - last_request > Duration(5.0))){
+            ROS_INFO("Waiting to complete takeoff...");
+        }
+        else{
+            ROS_INFO("Takeoff Completed Successfully!");
+            Duration(20).sleep();
+
+            Offboard* offboard = new Offboard();
+            offboard->init_connection();
+            offboard->offboard(2, 2, 2);
+            ROS_INFO("This Done");
+            // offboard->panorama();
+            // offboard->mission();
+            offboard->follow_stt();
+
+            flag = false;
+        }
+    }
+    // Duration(10).sleep();
+    Land* land = new Land();
+    land->init_connection();
+    land->land();
+
+    spin();
+    return 0;
+}
