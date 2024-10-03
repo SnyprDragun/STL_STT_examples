112c5dada4fca39e33df329994938ca22a9efb52
SnyprDragun <subho02.dc@gmail.com>
2024-10-03 23:02:59 +0530

    gamma sorted + uav_control added

--- src/STT_Controller.py
+++ src/STT_Controller.py
@@ -26,20 +26,17 @@ class STT_Controller():
         self.current_state = msg
 
     def gamma(self, t):
-        '''method to calculate tube boundaries at time instance 't' '''
-        real_tubes = np.zeros(2 * self.dimension)
-
-        for i in range(2 * self.dimension):
-            power = 0
-            for j in range(self.degree + 1):
-                real_tubes[i] += ((self.C[j + i * (self.degree + 1)]) * (t ** power))
-                power += 1
-        return real_tubes
+        '''method to calculate tube boundaries at time instance 't' using torch for optimization'''
+        t_tensor = torch.tensor(t, dtype=torch.float32)
+        powers_of_t = torch.tensor([t_tensor**j for j in range(self.degree + 1)], dtype=torch.float32)
+        C_tensor = torch.tensor(self.C, dtype=torch.float32).view(2 * self.dimension, self.degree + 1)
+        real_tubes = torch.matmul(C_tensor, powers_of_t)
+        return real_tubes.numpy()
     
     def normalized_error(self, x, gamma_sum, gamma_diff):
         return (2 * x - gamma_sum) / gamma_diff
 
-    def control(self):
+    def omnibot_control(self):
         """Calculates the error as the difference between the current state and the target."""
         rate = rospy.Rate(1)
 
@@ -48,8 +45,8 @@ class STT_Controller():
 
         while not rospy.is_shutdown():
             for t in t_values:
+                gamma = self.gamma(t)
                 if self.dimension == 2:
-                    gamma = self.gamma(t)
                     gamma_xl, gamma_yl, gamma_xu, gamma_yu = gamma[0], gamma[1], gamma[2], gamma[3]
                     
                     gamma_sx = gamma_xu + gamma_xl
@@ -86,6 +83,59 @@ class STT_Controller():
 
                     rate.sleep()
 
+    def uav_control(self):
+        """Calculates the error as the difference between the current state and the target."""
+        rate = rospy.Rate(1)
+
+        k = 5
+        t_values = np.arange(self.start, self.end + 1, 5)
+
+        while not rospy.is_shutdown():
+            for t in t_values:
+                gamma = self.gamma(t)
+                if self.dimension == 3:
+                    gamma_xl, gamma_yl, gamma_zl, gamma_xu, gamma_yu, gamma_zu = gamma[0], gamma[1], gamma[2], gamma[3], gamma[4], gamma[5]
+
+                    gamma_sx = gamma_xu + gamma_xl
+                    gamma_dx = gamma_xu - gamma_xl
+                    gamma_sy = gamma_yu + gamma_yl
+                    gamma_dy = gamma_yu - gamma_yl
+                    gamma_sz = gamma_zu + gamma_zl
+                    gamma_dz = gamma_zu - gamma_zl
+
+                    e1 = self.normalized_error(self.current_state.pose.pose.position.x, gamma_sx, gamma_dx)
+                    e2 = self.normalized_error(self.current_state.pose.pose.position.y, gamma_sy, gamma_dy)
+                    e3 = self.normalized_error(self.current_state.pose.pose.position.z, gamma_sz, gamma_dz)
+
+                    e_matrix = torch.tensor([e1, e2, e3])
+                    print("e_matrix: ", e_matrix)
+
+                    epsilon1 = math.log((1 + e1) / (1 - e1))
+                    epsilon2 = math.log((1 + e2) / (1 - e2))
+                    epsilon3 = math.log((1 + e3) / (1 - e3))
+
+                    epsilon_matrix = torch.tensor([epsilon1, epsilon2, epsilon3])
+                    gamma_d_matrix = torch.diag(torch.tensor([gamma_dx, gamma_dy, gamma_dz]))
+                    xi_matrix = 4 * torch.matmul(gamma_d_matrix.inverse(), (torch.eye(self.dimension) - torch.matmul(e_matrix.T, e_matrix)).inverse().to(torch.float64))
+                    u_matrix = - k * torch.matmul(xi_matrix, epsilon_matrix.to(torch.float64))
+
+                    v_x = u_matrix[0].item()
+                    v_y = u_matrix[1].item()
+                    v_z = u_matrix[2].item()
+
+                    vel_msg = Twist()
+                    if -0.22 < v_x < 0.22 and -0.22 < v_y < 0.22:
+                        vel_msg.linear.x = v_x
+                        vel_msg.linear.y = v_y
+                        self.vel_pub.publish(vel_msg)
+                    else:
+                        vel_msg.linear.x = 0.22
+                        vel_msg.linear.y = 0.22
+                        self.vel_pub.publish(vel_msg)
+
+                    rate.sleep()
+
+                
 if __name__ == '__main__':
     C0 = 0.5#20797808558189697
     C1 = 0.5#16574648035233522
@@ -103,29 +153,8 @@ if __name__ == '__main__':
     # C13 = -0.41057611837612756
     # C14 = 0.46889554184430615
     # C15 = -0.038642267452590924
-    C = [C0, C1, C2, C3]
+    C = [C0, C1, C2, C3]#, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15]
     try:
-        STT_Controller('/odom', '/cmd_vel', C, 0, 0, 30).control()
+        STT_Controller('/odom', '/cmd_vel', C, 0, 0, 30).uav_control()
     except rospy.ROSInterruptException:
         print("some error")
-
-
-
-# import torch
-
-# def gamma(self, t):
-#     '''method to calculate tube boundaries at time instance 't' using torch for optimization'''
-    
-#     # Convert t into a tensor
-#     t_tensor = torch.tensor(t, dtype=torch.float32)
-    
-#     # Prepare tensor for powers of t (i.e., [t^0, t^1, ..., t^degree])
-#     powers_of_t = torch.tensor([t_tensor**j for j in range(self.degree + 1)], dtype=torch.float32)
-    
-#     # Reshape the coefficients C and powers for matrix multiplication
-#     C_tensor = torch.tensor(self.C, dtype=torch.float32).view(2 * self.dimension, self.degree + 1)
-    
-#     # Perform the matrix multiplication between C_tensor and powers_of_t
-#     real_tubes = torch.matmul(C_tensor, powers_of_t)
-    
-#     return real_tubes.numpy()  # Convert back to numpy array if needed
